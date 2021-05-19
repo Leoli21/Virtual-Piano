34675fc595f1a72e9fb7aaa17c3c5d5650db57f2
Leoli21 <43002601+Leoli21@users.noreply.github.com>
2021-05-19 16:26:16 -0400

    Add files via upload

--- Piano.java
+++ Piano.java
@@ -5,11 +5,15 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
+import javax.swing.ButtonModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLayeredPane;
+import javax.swing.Timer;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
-public class Piano extends JFrame implements ActionListener, MouseListener
+public class Piano extends JFrame implements ActionListener
 {
 //	private PianoKeys2[][] piano; 
 	private ArrayList<PianoKeys2> piano;
@@ -21,7 +25,7 @@ public class Piano extends JFrame implements ActionListener, MouseListener
 	
 	//private double timeHeld; //Timer???
 	
-	final int octaves = 4;
+	final int octaves = 3;
 	
 	
 	//Option II
@@ -31,19 +35,23 @@ public class Piano extends JFrame implements ActionListener, MouseListener
 	final int wKeyWidth = 75;
 	final int bKeyWidth = 45;
 	
+	private ButtonModel model;
+	
 	private String[] whiteKeyNotations = {"C", "D", "E", "F", "G", "A", "B"};
 	private String[] blackKeyNotations = {"C#/Db", "D#/Eb", "F#/Gb", "G#/Ab", "A#/Bb"};
 	
 	public Piano()
 	{
-		this.setBounds(100,100,2100,500);
+		this.setBounds(100,100,wKeyWidth*7*octaves,500);
 		this.setLayout(null);
 		this.setTitle("Piano");
-		this.setResizable(false);
+		this.setResizable(true);
 		
+		Timer t1 = new Timer(25,this);
+		t1.start();
 		JLayeredPane panel = new JLayeredPane();
 		panel.setLayout(null);
-		panel.setBounds(0,0,2100,500);
+		panel.setBounds(0,0,wKeyWidth*7*octaves,500);
 			
 		piano = new ArrayList<PianoKeys2>();
 		
@@ -51,6 +59,7 @@ public class Piano extends JFrame implements ActionListener, MouseListener
 		//Constructing White Keys
 		for(int wKey = 0; wKey < octaves * 7; wKey++)
 		{
+			
 			piano.add(new PianoKeys2("White", whiteKeyNotations[wKey%7], whiteXPos));
 			whiteXPos+=wKeyWidth;
 		}
@@ -126,6 +135,8 @@ public class Piano extends JFrame implements ActionListener, MouseListener
 		for(int i = 0; i < piano.size(); i++)
 		{
 			PianoKeys2 key = piano.get(i);
+			key.getModel().addChangeListener(new BtnModelListener());
+			
 			if(key.getKeyType().equals("White"))
 			{
 				key.setBackground(Color.WHITE);
@@ -153,35 +164,39 @@ public class Piano extends JFrame implements ActionListener, MouseListener
 
 	}
 	
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseClicked(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+	private class BtnModelListener implements ChangeListener
+	{
+	    private boolean pressed = false;  // holds the last pressed state of the button
 
+	    @Override
+	    public void stateChanged(ChangeEvent e)
+	    {
+	        model = (ButtonModel) e.getSource();
+
+	        // if the current state differs from the previous state
+	        if (model.isPressed() != pressed)
+	        {
+	            String text = "Button pressed: " + model.isPressed() + "\n";
+	            pressed = model.isPressed();
+	             
+	        }
+	    }
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) 
+	{
+		for(int note = 0; note < piano.size(); note++)
+		{
+			ButtonModel model = piano.get(note).getModel();
+			model.addChangeListener(new BtnModelListener());
+			
+			if(model.isPressed())
+			{
+				System.out.println(note + " Was pressed!");
+			}
+		
+		}
+		
+	}
 }
+
